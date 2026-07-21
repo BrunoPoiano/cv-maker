b77ea3869dfe37db8d87cafddc9367ae13b57f9a
BrunoPoiano <bruno.poiano@proton.me>
2026-07-21 13:19:54 -0300

    Refactor core skills modal components
    Split the core skills modal into smaller, reusable components and
    standardize filename casing.

---
+++ src/components/menu/modals/coreSkills/components/coreSkillHeader.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+
+import { ProviderKey } from '@/keys'
+import SvgNewDocument from '@/svgs/svgNewDocument.vue'
+import AppButton from '@/ui/appButton.vue'
+import AppInput from '@/ui/appInput.vue'
+import AppPopover from '@/ui/appPopover.vue'
+import AppSmall from '@/ui/appSmall.vue'
+
+type Props = {
+	id: string
+}
+
+const props = defineProps<Props>()
+const { curriculum } = inject(ProviderKey)!
+</script>
+
+<template>
+	<div class="header">
+		<h3>
+			<AppInput
+				type="checkbox"
+				label="Core Skills"
+				v-model="curriculum.CoreSkills.show"
+			/>
+			<AppSmall>Items will be separeted by commas (,)</AppSmall>
+		</h3>
+		<AppPopover>
+			<AppButton modal icon-button :id="props.id">
+				<SvgNewDocument />
+			</AppButton>
+			<template #popover> New Core Skill </template>
+		</AppPopover>
+	</div>
+</template>
---
+++ src/components/menu/modals/coreSkills/components/coreSkillsOptions.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+
+import { fontSizeSelect } from '@/constants/font-size'
+import { ProviderKey } from '@/keys'
+import AppInput from '@/ui/appInput.vue'
+import AppSelect from '@/ui/appSelect.vue'
+
+const { curriculum } = inject(ProviderKey)!
+</script>
+
+<template>
+	<div class="settings">
+		<AppInput
+			type="checkbox"
+			label="Side by Side"
+			v-model="curriculum.CoreSkills.sideBySide"
+		/>
+		<AppSelect
+			label="Font Size"
+			:items="fontSizeSelect"
+			v-model="curriculum.CoreSkills.size"
+		/>
+	</div>
+</template>
+<style scoped>
+@layer utilities {
+	.settings {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		align-items: center;
+		gap: 1rem;
+		margin-bottom: 1rem;
+	}
+}
+</style>
